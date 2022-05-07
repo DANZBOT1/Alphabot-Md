54471c16e22486fccdbc8e3f18716930b6fd10e4
DANZBOT1 <102167087+DANZBOT1@users.noreply.github.com>
2022-05-08 02:35:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,22 +18,28 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "Alphabot-Mdོ" //AxelClouds Bot
+global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //DhanyGanteng
 global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.email = "zeeoneofc@gmail.com" //AxelNetwork@gmail.com
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "62887435047326" // "6285733374626"
+global.ownernomerr = "+62887435047326" // "+6285733374626"
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
-global.sessionName = 'session'
+global.packname = '© Alphabot-Mdོ' //AxelClouds
+global.author = 'Di Buat Oleh ZeeoneOfc' //DhanyBaek
+global.sessionName = '{
+	"clientID": "+HPfVQTe1HidaV7YQBZ/8A==",
+	"serverToken": "1@5I6WmmFSmrovb1IgzMllE8XbT2WQI3h9ccNBqzXSNkz1i9am65YHlGQJgVx0UMFFvzhz4GkKMPma7A==",
+	"clientToken": "v5eksTW+uGq/QjRk0PovI86zPfbCTjH0vccNmOMDvfc=",
+	"encKey": "qT05y1cco9EJTZI083cuQhmvBrEnnyXtTVyPYhF0cf0=",
+	"macKey": "PSP+h7tldZpXTnmbzXKUwqODtJ8a5Qr64v3GJEm5b9c="
+}'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
